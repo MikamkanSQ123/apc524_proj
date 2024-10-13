91f3d91932a212ad148ece82cb2bd4b82d6a179a
Mikamkan <siqingzou1@gmail.com>
2024-10-13 17:48:08 -0400

    fix function name and kwargs

--- data/datagenerator.py
+++ data/datagenerator.py
@@ -10,7 +10,7 @@ class Data(object):
 		self.sep = sep
   
 	@staticmethod
-	def BM(length=100, sep=0.01, mu=0, sigma=1):
+	def bm(length=100, sep=0.01, mu=0, sigma=1):
 		np.random.seed(0)
 		t = np.linspace(0, length*sep, length)
 		x = np.random.randn(length)
@@ -18,7 +18,7 @@ class Data(object):
 		return t*mu + sigma*W
 
 	@staticmethod
-	def OU(length=100, sep=0.01, lam=1, mu=0, sigma=1):
+	def ou(length=100, sep=0.01, lam=1, mu=0, sigma=1):
 		np.random.seed(0)
 		t = np.linspace(0, length*sep, length)
 		x = np.random.randn(length)
@@ -26,26 +26,23 @@ class Data(object):
 		return mu + np.exp(-lam*t) * (W - mu) + sigma * np.exp(-2*lam*t) * np.random.randn(length)
 
 	@staticmethod
-	def GBM(length=100, sep=0.01, mu=0, sigma=1):
+	def gbm(length=100, sep=0.01, mu=0, sigma=1):
 		np.random.seed(0)
 		t = np.linspace(0, length*sep, length)
 		x = np.random.randn(length)
 		W = np.cumsum(x) * np.sqrt(sep)
 		return np.exp((mu - 0.5*sigma**2)*t + sigma*W)
 
-	def stock(self, **kwargs): # default GBM
-		rf = kwargs.get('rf', 0.03)
-		div = kwargs.get('div', 0)
-		vol = kwargs.get('vol', 0.2)
-		return self.GBM(self.length, mu=rf-div, sigma=vol)
+	def stock(self, rf=0.03, div=0, vol=0.2): # default GBM
+		return self.gbm(self.length, mu=rf-div, sigma=vol)
 
 
 if __name__ == '__main__':
 	data = Data(length=20)
 	print(data.stock())
 	print(data.stock(rf=0.05, div=0.02, vol=0.3))
-	print(data.BM())
-	print(data.OU())
-	print(data.GBM())
+	print(data.bm())
+	print(data.ou())
+	print(data.gbm())
 	
 	
