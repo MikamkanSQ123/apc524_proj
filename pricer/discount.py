649e61d08f5628d4d6381f22304e1a10d4cd56ae
zhanghq-mbp2023 <zhanghq.chn@gmail.com>
2024-10-13 16:45:52 -0400

    feature: simple compounding

--- pricer/discount.py
+++ pricer/discount.py
@@ -1,7 +1,11 @@
 import math
 
-def discount(r, t):
-    return math.exp(-r * t)
+def discount(r, t, freq=None):
+    if freq is None:
+        return math.exp(-r * t)
+    else:
+        return 1 / (1 + r / freq) ** (freq * t)
 
 if __name__ == "__main__":
     assert(abs(discount(0.05, 1) - 0.9512294) < 1e-4)
+    assert(abs(discount(0.05, 1, 2) - 0.9518144 < 1e-4))
