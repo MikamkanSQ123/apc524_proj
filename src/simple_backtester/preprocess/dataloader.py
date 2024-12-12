fa1ff96071fa6f2df283b6e3935a1f2c4e390711
Mikamkan <siqingzou1@gmail.com>
2024-12-11 19:22:55 -0500

    mv data + new source for dataloader

--- src/simple_backtester/preprocess/dataloader.py
+++ src/simple_backtester/preprocess/dataloader.py
@@ -2,9 +2,12 @@ import pandas as pd
 from typing import Any, Union
 from .techlib import Techlib
 from pathlib import Path
+import ccxt  # type: ignore[import-not-found]
 
-path = "./src/simple_backtester/data/feature/"
+source = "ccxt"
+path = f".src/tests/test_data/data/{source}/feature/"
 config = {
+    "source": source,
     "data_path": path,
     # "tech_indicators": ["ma", "macd", "rsi"],
     "features": [file.name[:-4] for file in Path(path).iterdir() if file.is_file()],
@@ -65,7 +68,6 @@ class DataLoader:
 
         features_dict: dict[str, Any] = {}
         for feature in features:
-            features_dict[feature] = None
             # load data from local files if it already exists in path
             if feature in self.config["features"]:
                 self.load_from_file(feature)
@@ -84,10 +86,108 @@ class DataLoader:
                 features_dict[name] = eval(
                     f"Techlib.{feature}(self.data[base][symbols], *farg)"
                 ).loc[start:end]  # type: ignore[misc]
+        # For not found features
+        features_not_found = [
+            f
+            for f in features
+            if (f not in self.config["features"])
+            and (f not in self.config["tech_indicators"])
+        ]
+        print(f"Features not found: {features_not_found}")
+        if self.config["source"] == "ccxt":
+            print("Fetching data from ccxt...")
+            dict_not_found = ccxtFetcher.load_data(
+                start, end, symbols, features_not_found
+            )
+            self.data.update(dict_not_found)
+            features_dict.update(dict_not_found)
         return features_dict
 
     def _test(self) -> Union[pd.DataFrame, "pd.Series[Any]"]:
         return Techlib.ma(self.data["BTC"], 10)
+
+
+class ccxtFetcher(object):
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def is_feasible(exchange: Union[str, ccxt.Exchange], func: str) -> bool:
+        try:
+            if isinstance(exchange, str):
+                exchange = getattr(ccxt, exchange)()
+            if exchange.has[func]:
+                return True
+        except Exception:
+            # Handle exceptions (e.g., API errors, unsupported methods)
+            return False
+        return False
+
+    @staticmethod
+    def get_time(
+        start: Union[str, pd.Timestamp], end: Union[str, pd.Timestamp], freq: str = "D"
+    ) -> Any:
+        return pd.date_range(start, end, freq=freq).strftime("%Y-%m-%d").tolist()
+
+    @staticmethod
+    def get_tick(
+        exchange: Union[str, ccxt.Exchange],
+        symbol: str,
+        date: str,
+        timeframe: str = "1m",
+        bars: int = 100000,
+    ) -> Any:
+        if isinstance(exchange, str):
+            exchange = getattr(ccxt, exchange)()
+        since = exchange.parse8601(f"{date}T00:00:00Z")
+        # since = exchange.parse8601(date)
+        assert ccxtFetcher.is_feasible(exchange, "fetchOHLCV")
+        ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since, bars)
+
+        df = pd.DataFrame(
+            ohlcv, columns=["timestamp", "open", "high", "low", "close", "volume"]
+        )
+        df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+        df.set_index("timestamp", inplace=True)
+        return df
+
+    @staticmethod
+    def get_data(
+        exchange: Union[str, ccxt.Exchange],
+        symbols: list[str],
+        start: Union[str, pd.Timestamp],
+        end: Union[str, pd.Timestamp],
+        timeframe: str = "1m",
+    ) -> Any:
+        time = ccxtFetcher.get_time(start, end)
+        dfs = pd.DataFrame()
+        for t in time:
+            for symbol in symbols:
+                try:
+                    df = ccxtFetcher.get_tick(exchange, symbol, t, timeframe)
+                    df["symbol"] = symbol
+                    dfs = pd.concat([dfs, df], axis=0)
+                except Exception as e:
+                    print(f"Error: {e} when running {exchange} {symbol} {t}")
+        return dfs
+
+    @staticmethod
+    def load_data(
+        start: Union[str, pd.Timestamp],
+        end: Union[str, pd.Timestamp],
+        symbols: list[str],
+        features: list[str],
+        exchange: str = "binanceus",
+        timeframe: str = "1m",
+    ) -> Any:
+        possible_features = ["open", "high", "low", "close", "volume"]
+        fts = [f for f in features if f in possible_features]
+        df = ccxtFetcher.get_data(exchange, symbols, start, end, timeframe)
+        features_dict = {
+            col: df.pivot(columns="symbol", values=col)[start:end]  # type: ignore[misc]
+            for col in fts
+        }
+        return features_dict
 
 
 if __name__ == "__main__":
@@ -105,5 +205,13 @@ if __name__ == "__main__":
         "2024-11-11 23:54:00",
         ["BTC", "ZRX"],
         ["price", "return"],
+    )
+    # only if source is ccxt
+    features = dl.load_data(
+        "2022-01-01",
+        "2022-01-02",
+        ["1INCH/USDT", "1INCH/USD"],
+        ["open", "close"],
+        base="close",
     )
     # print(features['price'].iloc[2:4,:])
